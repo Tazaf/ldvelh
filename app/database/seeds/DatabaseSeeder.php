eb4933307e6db56599a02af72c59090bc8dfaa28
Tazaf <obersonmathias@gmail.com>
2014-06-08 17:09:37 +0200

    Revert "first attempt"
    This reverts commit 1f5fe57ed4266390c5a8d2e9ba76267776f208ff.

--- app/database/seeds/DatabaseSeeder.php
+++ app/database/seeds/DatabaseSeeder.php
@@ -2,14 +2,16 @@
 
 class DatabaseSeeder extends Seeder {
 
-    public function run() {
-        Eloquent::unguard();
+	/**
+	 * Run the database seeds.
+	 *
+	 * @return void
+	 */
+	public function run()
+	{
+		Eloquent::unguard();
 
-        $this->call('TypeTableSeeder');
-        $this->command->info('Type table seeded!');
-
-        $this->call('PossessionTableSeeder');
-        $this->command->info('Possession table seeded!');
-    }
+		// $this->call('UserTableSeeder');
+	}
 
 }
